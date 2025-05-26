f9303c3cc9fbe0a77ea3995e42ebff9ceed653fc
MSudhakar517 <charancherry9833@gmail.com>
2025-05-26 09:25:09 +0530

    Sample exercise for HomeWork Submit

---
+++ src/com/demo/pattern/Demo3.java
@@ -0,0 +1,12 @@
+package com.demo.pattern;
+
+public class Demo3 {
+	
+	public static void main(String[] args) {
+		
+		for (int i =1;i<=3;i++) {//number of iterations 3
+			System.out.println(i);
+		}
+	}
+
+}
